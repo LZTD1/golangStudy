1d2954d22abaa3a46e6236a56f7ba810db32f3aa
pavlov <danil227pavlov@gmail.com>
2024-11-18 13:02:21 +0300

    Реализация 1 ДЗ

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"goStudy/pkg/crawler"
+	"goStudy/pkg/crawler/spider"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello Goland")
+	f := flag.String("s", "", "Contains word")
+	flag.Parse()
+	if *f != "" {
+		fmt.Printf("Установлен флаг - %s\n", *f)
+	}
+
+	crawl := spider.New()
+
+	ch := make(chan []crawler.Document)
+
+	go func(ch chan []crawler.Document) {
+		fmt.Println("Запуск crawler`а по https://go.dev")
+		r, _ := crawl.Scan("https://go.dev", 2)
+		ch <- r
+	}(ch)
+	go func(ch chan []crawler.Document) {
+		fmt.Println("Запуск crawler`а по https://devdocs.io/go")
+		r, _ := crawl.Scan("https://devdocs.io/go/", 2)
+		ch <- r
+	}(ch)
+
+	r := []crawler.Document{}
+	for i := 0; i < 2; i++ {
+		select {
+		case docs := <-ch:
+			for _, doc := range docs {
+				if strings.Contains(strings.ToLower(doc.Title), *f) {
+					r = append(r, doc)
+				}
+			}
+		}
+	}
+
+	fmt.Printf("Собрано %d ссылок", len(r))
 }
